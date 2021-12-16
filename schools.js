aac517ce01f91b0aaedd5c3012caba41981f5d6d
Israr Khan <khan.israr.lap.7651@gmail.com>
2021-12-16 11:32:22 +0530

    my name is israr khan and i have added a dot in your schools.js file

--- schools.js
+++ schools.js
@@ -1,6 +1,5 @@
 const schoools = {
-  Perennialism:
-    "The perennial philosophy, also referred to as perennialism and perennial wisdom, is a perspective in philosophy and spirituality that views all of the world's religious traditions as sharing a single, metaphysical truth or origin from which all esoteric and exoteric knowledge and doctrine has grown",
+  Perennialism: "The perennial philosophy, also referred to as perennialism and perennial wisdom, is a perspective in philosophy and spirituality that views all of the world's religious traditions as sharing a single, metaphysical truth or origin from which all esoteric and exoteric knowledge and doctrine has grown.",
 };
 
 export default schoools;
